401fa87e1c8e739a35e69fa67bd9c003329ed26b
c2064895 <c2064895@newcastle.ac.uk>
2024-07-03 15:59:35 +0100

    Update move.sh
    Fixed an error where it will copy from OS file directly.
    Added checks and status reporting.

--- move.sh
+++ move.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-cp -a /lib/. $1
+if [ "$#" -ne 1 ]; then
+    echo "Syntax: move.sh <target_dist>"
+fi
+
+cp -a ./lib/. $1
+if ["$#" -eq 0]; then
+  echo "Copy compelete!"
+else
+  echo "Copy error! $# was returned"
+fi
+
