bd0812c01493ede3c08d624b9c22c19774465e51
abdul algham <megacryleon@gmail.com>
2024-07-03 19:10:09 +0100

    fixed move.sh

--- move.sh
+++ move.sh
@@ -2,12 +2,16 @@
 
 if [ "$#" -ne 1 ]; then
     echo "Syntax: move.sh <target_dist>"
+    exit 1
 fi
 
 cp -a ./lib/. $1
 if ["$#" -eq 0]; then
-  echo "Copy compelete!"
+  echo "Copy is compelete!"
 else
   echo "Copy error! $# was returned"
 fi
+
+
+
 
